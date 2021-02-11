44756e786cdc21ad1549093a7d41d1fa640011ad
Guilherme Silva Antunes <guilherme.silva.antunes1999@gmail.com>
2021-02-11 17:36:55 -0300

    closes #5, time getter ok (helper)

--- utils/helper.py
+++ utils/helper.py
@@ -19,3 +19,8 @@ def verify_file(chosen_path: str) -> bool:
 @path_exists
 def verify_directory(chosen_path: str) -> bool:
     return path.isdir(chosen_path)
+
+
+def get_time() -> str:
+    time_now = datetime.now()
+    return time_now.strftime('%b-%d-%Y_%H-%M-%S')
